8713767ad03783d07cf1bdf74d1afb1edabb3b9c
rakshitkoyani <36806953+rakshitkoyani@users.noreply.github.com>
2020-08-27 21:53:07 +0530

    Initial commit

---
+++ js/master_js_12.js
@@ -0,0 +1,263 @@
+
+
+
+<head>
+
+ 
+        <!-- HTML Meta Tags -->
+        <title> || DIGI CARD || Digital Visiting Card Online </title>
+       
+
+        <!-- Facebook Meta Tags -->
+		 <meta property="og:image" content>
+
+        <meta property="og:url" content="https://meradigicard.com/">
+        <meta property="og:type" content="website">
+        <meta property="og:title" content=" || Digital Visiting Card Online">
+        <meta property="og:description" content=" ">
+       
+        <!-- Twitter Meta Tags -->
+		 <meta name="twitter:image" content>
+
+        <meta property="twitter:url" content="https://meradigicard.com/">
+        <meta name="twitter:card" content="summary_large_image">
+        <meta name="twitter:title" content=" || Digital Visiting Card Online">
+        <meta name="twitter:description" content=" ">
+       
+        <!-- Meta Tags Generated via -->
+      
+		<link rel="icon" href type="image/*" sizes="16x16">
+
+ 
+
+  <!-- Required meta tags -->
+		  <meta charset="utf-8">
+		  <!-- Required meta tags -->
+		  <meta charset="utf-8">
+		  <link rel="stylesheet" href="../css/all_12.css" crossorigin="anonymous">
+		  <link rel="stylesheet" href="../css/awesome.min_12.css">
+  
+ <!-- Required css  -->
+		 <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
+
+		<link rel="stylesheet" href="../css/css_13.css">
+		<link rel="stylesheet" href="../css/mobile_css_12.css">
+		<script src="master_js_12.js"></script>
+
+
+
+
+
+<meta http-equiv="refresh" content="100;URL=../index.php"><link rel="stylesheet" href="../css/card_css1_11.css">
+
+<script>
+
+$(document).ready(function(){
+	$('.mobile_home').on('click',function(){
+		$('#header').toggleClass('add_height');
+		
+	})
+})
+
+</script>
+
+<style>
+.full_page_alert {position: fixed;
+    width: -webkit-fill-available;
+    height: -webkit-fill-available;
+    background: white;
+    top: 0;
+    z-index: 9999999;
+    padding: 63px;
+    text-align: center;}
+
+</style>
+
+
+<script>
+	
+
+</script>
+<!----------------------copy from here ------------------------->
+
+
+	<div class="card" id="home">
+	
+	<div class="view_counter"><i class="fa fa-eye"></i> <br>1784</div>			
+			<div class="card_content"><img src alt="Logo"></div>
+			<div class="card_content2">
+				<h2></h2>
+				<p></p>
+				<p></p>
+				
+			</div>
+			<div class="dis_flex">
+												
+				
+				
+															
+			</div>
+	
+			<div class="contact_details">
+																				
+			</div>
+			
+			<div class="dis_flex">
+				<div class="share_wtsp">
+					<form action="https://api.whatsapp.com/send" id="wtsp_form" target="_blank"><input type="text" name="phone" placeholder="WhatsApp Number with Country code	" value="+91"><input type="hidden" name="text" value="https://meradigicard.com/"><div class="wtsp_share_btn" onclick="subForm()"><i class="fa fa-whatsapp"></i> Share</div></form>
+					
+					<script>
+					
+					$(document).ready(function(){
+						$('.wtsp_share_btn').on('click',function(){
+							$('#wtsp_form').submit();
+						})
+						
+					})
+					</script>
+				</div>
+			</div>
+			
+			<div class="dis_flex">
+			
+							
+				<div class="big_btns" id="share_box_pop">Share <i class="fa fa-share-alt"></i></div>
+			
+				<div class="share_box">
+				
+				
+				<div class="close" id="close_sharer">&times;</div>
+				<p>Share My Digital Card </p>
+						<a href="https://api.whatsapp.com/send?text=https://meradigicard.com/"><div class="shar_btns"><i class="fa fa-whatsapp" id="whatsapp2" target="_blank"></i><p>WhatsApp</p></div></a>
+					<a href="sms:?body=https://meradigicard.com/" target="_blank"><div class="shar_btns"><i class="fa fa-comment"></i><p>SMS</p></div></a>
+					
+					<a href="https://www.facebook.com/sharer/sharer.php?u=https://meradigicard.com/" target="_blank"><div class="shar_btns"><i class="fa fa-facebook"></i><p>Facebook</p></div></a>
+					<a href="https://twitter.com/intent/tweet?text=https://meradigicard.com/" target="_blank"><div class="shar_btns"><i class="fa fa-twitter"></i><p>Twitter</p></div></a>
+					<a href target="_blank"><div class="shar_btns"><i class="fa fa-instagram"></i><p>Instagram</p></div></a>
+					<a href="https://www.linkedin.com/cws/share?url=https://meradigicard.com/" target="_blank"><div class="shar_btns"><i class="fa fa-linkedin"></i><p>Linkedin</p></div></a>
+				</div>
+			
+				<script>
+					$(document).ready(function(){
+						$('#close_sharer,#share_box_pop').on('click',function(){
+							$('.share_box').slideToggle();
+						});
+					})
+				
+				
+				</script>
+			
+			</div>
+			<div class="dis_flex">
+			
+																											</div>
+			
+			
+			
+	
+	</div>
+	
+	<div class="card2" id="scan_qr">
+	
+	<h3>Scan QR Code to download the contact details</h3>
+	
+	
+	<img src="../chart_1" id="qr_code_d">
+	
+	</div>
+	
+	
+<!--------------about us --------------------------->	
+	
+	<div class="card2" id="about_us">
+		<h3>About Us</h3>
+					
+		
+	
+	</div>
+	
+<!------------shopping online-------------------------->
+
+
+	
+		
+	
+	
+<!--------------youtube videos--------------------------->	
+
+	
+	
+		
+<!----------product and services ----------------------->		
+
+
+		
+<!----------image gallery----------------------->		
+
+
+		
+<!----------payment info----------------------->	
+	
+	
+	
+<!----------email to  info----------------------->	
+	<div class="card2" id="enquery">
+		
+		<form action="#" method="post">
+			<h3>Contact Us</h3>
+			
+			<input type name="c_name" placeholder="Enter Your Name" required>
+			<input type name="c_contact" maxlength="13" placeholder="Enter Your Mobile No" required>
+			<input type="email" name="c_email" placeholder="Enter Your Email Address">
+			<textarea name="c_msg" placeholder="Enter your Message or Query" required></textarea>
+			<input type="submit" Value="Send!" name="email_to_client">
+		
+		</form>
+		
+	<br>
+	
+		<a href="index.php"><div class="create_card_btn">meradigicard.com|| Create Your Card Now || 2020</div></a>		
+	<style>
+	.create_card_btn {
+		         background: linear-gradient(45deg, black, black);
+    color: white;
+    width: auto;
+    padding: 20px;
+    border-radius: 2px;
+    line-height: 0.8;
+    margin: 11px auto;
+    font-size: 9px;
+    text-align: center;
+	}
+	
+	
+	
+#svg_down{position: fixed;
+    bottom: 0;
+    z-index: -1;
+    left: 0;}
+
+	
+	</style>
+	
+	
+	
+	<br>
+	<br>
+	<br>
+	<br>
+	<div class="menu_bottom">
+		<div class="menu_container">
+			<div class="menu_item" onclick="location.href='#home'"><i class="fa fa-home"></i> Home</div>
+			<div class="menu_item" onclick="location.href='#about_us'"><i class="fa fa-briefcase"></i>About Us</div>
+			<div class="menu_item" onclick="location.href='#product_services'"><i class="fa fa-ticket"></i>Product & Services</div>
+			<div class="menu_item" onclick="location.href='#shop_online'"><i class="fa fa-archive"></i>Shop Online</div>
+			<div class="menu_item" onclick="location.href='#gallery'"><i class="fa fa-image"></i>Gallery</div>
+			<div class="menu_item" onclick="location.href='#youtube_video'"><i class="fa fa-video-camera"></i>Youtube Videos</div>
+			<div class="menu_item" onclick="location.href='#payment'"><i class="fa fa-money"></i>Payment</div>
+			<div class="menu_item" onclick="location.href='#enquery'"><i class="fa fa-comment"></i>Enquery</div>
+		</div>
+	</div>
+
+
+</div></head>
